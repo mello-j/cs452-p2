b920f5ccfd1a54fbf30d37da79e99acbd505362d
Justin M <88345763+mello-j@users.noreply.github.com>
2025-02-25 03:53:07 +0000

    finish get prompt

--- src/lab.c
+++ src/lab.c
@@ -19,28 +19,38 @@
  * This function is called with a cmd-line argument "v".
  * It will output the current version of the shell, and exit the program.
  */
-void print_vers()
-{
+void print_vers(){
     printf("Justin Shell version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
     exit(0);
 }
 
-char *get_prompt(const char *env)
-{
-    // Stub implementation
-    UNUSED(env);
-    return strdup("shell>");
+/**
+ * Task 4 - Get Prompt
+ * This function will return the prompt to be displayed in the shell.
+ * If the environment variable is not set, the default prompt will be used.
+ * @param env - the environment variable
+ * @return - the prompt to be displayed
+ */
+char *get_prompt(const char *env){
+    //get the environment variable
+    char *prompt = getenv(env);
+
+    if (prompt != NULL) {
+        //prompt exists so return it
+        return strdup(prompt);     
+    } else {
+          //no prompt so return default
+          return strdup("justinshell>");
+    }
 }
 
-int change_dir(char **dir)
-{
+int change_dir(char **dir){
     // Stub implementation
     UNUSED(dir);
     return 0;
 }
 
-char **cmd_parse(char const *line)
-{
+char **cmd_parse(char const *line){
     // Stub implementation
     UNUSED(line);
     return NULL;
@@ -51,16 +61,13 @@ char **cmd_parse(char const *line)
  * This function will free the memory allocated for command arguments.access
  * @param line - the command arguments
  */
-void cmd_free(char **line)
-{
-    if (line == NULL)
-    {
+void cmd_free(char **line){
+    if (line == NULL){
         return;
     }
 
     // Free each argument
-    for (size_t i = 0; line[i] != NULL; i++)
-    {
+    for (size_t i = 0; line[i] != NULL; i++){
         free(line[i]);
     }
 
@@ -74,8 +81,7 @@ void cmd_free(char **line)
  * @param line - the command arguments
  * @return - the command arguments with white space removed
  */
-char *trim_white(char *line)
-{
+char *trim_white(char *line){
     if (line == NULL){
         return;
     }
@@ -102,26 +108,29 @@ char *trim_white(char *line)
     return line;
 }
 
-bool do_builtin(struct shell *sh, char **argv)
-{
+bool do_builtin(struct shell *sh, char **argv){
     // Stub implementation
     UNUSED(sh);
     UNUSED(argv);
     return false;
 }
 
-void sh_init(struct shell *sh)
-{
+void sh_init(struct shell *sh){
     // Stub implementation
     UNUSED(sh);
 }
 
-void sh_destroy(struct shell *sh)
-{
+void sh_destroy(struct shell *sh){
     // Stub implementation
     UNUSED(sh);
 }
 
+/**
+ * Task 2 & 4 - Parse Arguments
+ * This function will parse the command line arguments
+ * @param argc - the number of arguments
+ * @param argv - the arguments
+ */
 void parse_args(int argc, char **argv){
    int opt;
 
